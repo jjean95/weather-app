5180c02e0d46662fd926ecd9fbe992de33f8fc99
Goh Jia Ean <jjeangoh@gmail.com>
2023-09-07 22:30:46 +0800

    modify code for icon

--- src/index.js
+++ src/index.js
@@ -58,6 +58,12 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
 }
 
 function searchCity(city) {
